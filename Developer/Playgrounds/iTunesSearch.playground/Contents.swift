98288425ad1c2e85fe441f36c542ae071b3f66f7
klsumsion-afk <klsumsion@gmail.com>
2025-11-10 10:07:25 -0700

    finished iTunes Search Parts 1 and 2

--- Developer/Playgrounds/iTunesSearch.playground/Contents.swift
+++ Developer/Playgrounds/iTunesSearch.playground/Contents.swift
@@ -5,12 +5,9 @@ import PlaygroundSupport
 
 var greeting = "Hello, playground"
 
-var components = URLComponents(string: "https://itunes.apple.com/search")!
-components.queryItems = [
-    "term": "Taylor+Swift",
-    "media": "music",
-    "limit": "1"
-].map { URLQueryItem(name: $0.key, value: $0.value) }
+enum StoredItemError: Error, LocalizedError {
+    case itemsNotFound
+}
 
 struct StoreItem: Codable {
     let artistName: String
@@ -18,27 +15,44 @@ struct StoreItem: Codable {
     let kind: String
     let primaryGenreName: String
     
-    enum CodingKeys: String, CodingKey {
-        case artistName
-        case trackName
-        case kind
-        case primaryGenreName
-    }
-    
-    init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.artistName = try container.decode(String.self, forKey: .artistName)
-        self.trackName = try container.decode(String.self, forKey: .trackName)
-        self.kind = try container.decode(String.self, forKey: .kind)
-        self.primaryGenreName = try container.decode(String.self, forKey: .primaryGenreName)
-    }
+//    enum CodingKeys: String, CodingKey {
+//        case artistName
+//        case trackName
+//        case kind
+//        case primaryGenreName
+//    }
+//    
+//    init(from decoder: any Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.artistName = try container.decode(String.self, forKey: .artistName)
+//        self.trackName = try container.decode(String.self, forKey: .trackName)
+//        self.kind = try container.decode(String.self, forKey: .kind)
+//        self.primaryGenreName = try container.decode(String.self, forKey: .primaryGenreName)
+//    }
 }
 
 struct SearchResponse: Codable {
     let results: [StoreItem]
 }
 
-func fetchItems(matching query: [String: String]) async throws -> [StoreItem]
+func fetchItems(matching query: [String: String]) async throws -> [StoreItem] {
+    var components = URLComponents(string: "https://itunes.apple.com/search")!
+    components.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+
+
+let (data, response) = try await URLSession.shared.data(
+    from: components.url!
+)
+
+guard let httpResponse = response as? HTTPURLResponse,
+      httpResponse.statusCode == 200 else { throw StoredItemError.itemsNotFound }
+data.prettyPrintedJSONString()
+
+let decoder = JSONDecoder()
+let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+
+return searchResponse.results
+}
 
 extension Data {
     func prettyPrintedJSONString() {
@@ -57,13 +71,26 @@ extension Data {
     }
 }
 
+
 Task {
-    let (data, response) = try await URLSession.shared.data(
-        from: components.url!
-    )
-    if let httpResponse = response as? HTTPURLResponse,
-       httpResponse.statusCode == 200 {
-        data.prettyPrintedJSONString()
+    do {
+        let storeItems = try await fetchItems(matching: query)
+        storeItems.forEach { item in
+            print("""
+                artistName: \(item.artistName)
+                trackName: \(item.trackName)
+                kind: \(item.kind)
+                primaryGenreName: \(item.primaryGenreName)
+            
+            """)
+        }
+    } catch {
+        print(error)
     }
 }
 
+let query = [
+    "term": "Taylor+Swift",
+    "media": "music",
+    "limit": "3"
+]
